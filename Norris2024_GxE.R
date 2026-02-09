86617362d0e0b575009bb50caf4cf39ea0edba6f
emmawhite15 <emmaw15@vt.edu>
2026-02-09 13:32:26 -0500

    Added code to Norris2024 script. Edited README.

--- Norris2024_GxE.R
+++ Norris2024_GxE.R
@@ -15,6 +15,17 @@ View(norris_dat_filtered)
 norris_dat_filtered_new <- norris_dat_filtered[-c(12,13)]
 View(norris_dat_filtered_new)
 
+# Remove NAs from the entire dataset
+norris_dat_filtered_new <- na.omit(norris_dat_filtered_new)
+
+#### Practice with using group_by, summarise, and mutate #####
+# Take mean and standard deviation of hatchling max speed
+norris_dat_filtered_new %>%
+  group_by(parental_island) %>%
+  summarise(hatchling_max_speed_avg = mean(hatchling_max_speed), hatchling_max_speed_sd = sd(hatchling_max_speed))
+# Add column for the natural log of hatchling svl
+norris_dat_filtered_hatchsvlln <- norris_dat_filtered_new %>% mutate(hatch_svl_ln = log(hatch_svl))
+View(norris_dat_filtered_hatchsvlln)
 
 
 
